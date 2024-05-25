72a5180ccb56f5e6cc5210e1efa40ec07f23ca1d
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-05-25 07:28:38 +0700

    Add Equals override

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +21,19 @@ namespace ServiceContracts.DTO
         //It compares the current object to another object
         // of CountryResponse type and returns true,
         // if both values are same; otherwise returns false
+        //public override bool Equals(object? obj)
+        //{
+        //    if (obj is null) return false;
+        //    if (obj.GetType() != typeof(CountryResponse))
+        //        return false;
+        //    CountryResponse country = (CountryResponse)obj;
+        //    return CountryId == country.CountryId
+        //        && CountryName == country.CountryName;
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return base.GetHashCode();
+        //}
     }
 
     //extension method to Country 
